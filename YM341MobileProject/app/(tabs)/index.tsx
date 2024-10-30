a634b3c454029fe17c6e842611828b378fbdf92b
Mustafa Akdaş <makdas163@gmail.com>
2024-10-30 16:57:00 +0300

    Changed html to react

--- YM341MobileProject/app/(tabs)/index.tsx
+++ YM341MobileProject/app/(tabs)/index.tsx
@@ -1,9 +1,7 @@
 import { Image, StyleSheet, Platform } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+
 
 export default function HomeScreen() {
   return (
@@ -15,39 +13,36 @@ export default function HomeScreen() {
           style={styles.reactLogo}
         />
       }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Lannnnnn</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+        <h1>Sign up</h1>
+    <p>Lütfen boşlukları gerekli bilgiler ile doldurunuz</p>
+    <form method="post" action='login.html'>
+      <fieldset>
+        <label typeof="name">Adinizi ve soyadinizi giriniz: <input id="name" name="name" type="text" required /></label>
+        <label typeof="username">Kullanici adinizi giriniz: <input id="username" name="username" type="text" required /></label>
+        <label typeof="email">Email adresinizi giriniz: <input id="email" name="email" type="email" required /></label>
+        <label typeof="password">Bir şifre oluşturunuz: <input id="password" name="password" type="password" pattern="[a-z0-5]{8,}" required /></label>
+        <span className="toggle-password" id="togglePassword">👁️</span>
+      </fieldset>
+      <fieldset>
+        <legend>Hesap türü (Zorunlu)</legend>
+        <label typeof="personal-account"><input id="personal-account" type="radio" name="account-type" className="inline" checked /> Kişisel</label>
+        <label typeof="business-account"><input id="business-account" type="radio" name="account-type" className="inline" /> Kurumsal</label>
+      </fieldset>
+      <fieldset>
+        <label typeof="profile-picture">Bir profil resmi yükleyiniz: <input id="profile-picture" type="file" name="file" /></label>
+        <label typeof="age">Yaşınızı giriniz: <input id="age" type="number" name="age" min="13" max="120" /></label>
+        
+        <label typeof="bio">Kendinizi tanıtınız:
+          <textarea id="bio" name="bio" placeholder="Ben 3.sınıf yazılım öğrencisyim..."></textarea>
+        </label>
+      </fieldset>
+      <label typeof="terms-and-conditions">
+        <input className="inline" id="terms-and-conditions" type="checkbox" required name="terms-and-conditions" /><a href="terms.html">Kullanıcı sözleşmesini</a>`ni okudum ve onaylıyorum
+      </label>
+      <input type="submit" value="Kayıt" />
+    </form>
+    <script src="login.js"></script>
+      </ParallaxScrollView>)
 }
 
 const styles = StyleSheet.create({
