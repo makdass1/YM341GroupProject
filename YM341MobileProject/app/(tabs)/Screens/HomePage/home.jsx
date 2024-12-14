1138132025379856029752388b754b15b904dee0
l0rz1x <150855591+l0rz1x@users.noreply.github.com>
2024-12-14 16:36:43 +0300

    home page added

---
+++ YM341MobileProject/app/(tabs)/Screens/HomePage/home.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/header";
+import './home.css'
+const Home = () => {
+    return(
+        <>
+         <Header />
+         <h2>home Page</h2>
+        </>
+    )
+}
+
+export default Home;
